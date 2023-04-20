81da98c0c44892df5e8eba96a3720ac1fcf2d3a6
Jordan Rollins <carpeomniavino@gmail.com>
2023-04-20 13:39:00 +0200

    small mods

--- client/scne/src/components/BusinessProfile.js
+++ client/scne/src/components/BusinessProfile.js
@@ -3,15 +3,23 @@
 const BusinessProfile = ({ name, bio, city, address, phone, reviewCount, ig, photo }) => {
     return (
       <>
-        <div>
-          <h2>{name}</h2>
+        <div className="businessProfileContainer">
+          <div className="businessProfileContent">
+          <div>
+          <h2>{name}</h2><div>{ig}</div>
+          </div>
+          <img src={photo} alt={`${name}'s photo`} />
+          
           <div>{bio}</div>
+          <div>
           <div>{city}</div>
           <div>{address}</div>
           <div>{phone}</div>
+          </div>
           <div>{reviewCount}</div>
-          <div>{ig}</div>
-          <img src={photo} alt={`${name}'s photo`} />
+          {/* team */}
+          </div>
+          
         </div>
       </>
     );
