edc8881dc04e57e6a0862866b0e153cb9a96e845
Jordan Rollins <carpeomniavino@gmail.com>
2023-04-20 20:21:22 +0200

    conditional render of divider

--- client/scne/src/components/ProfileUser.js
+++ client/scne/src/components/ProfileUser.js
@@ -1,33 +1,35 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getProfile } from '../api-service';
-
+import { Divider, Chip } from '@mui/material';
 
 const ProfileUser = () => {
-    const { id } = useParams();
-    const [user, setUser] = useState(null);
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
 
-    
+  useEffect(() => {
+    getProfile(id).then((data) => setUser(data));
+  }, [id]);
 
-    useEffect(() => {
-      getProfile(id).then((data) => setUser(data));
-    }, [id]);
-  
-    if (!user) {
-      return <div>Uh. oh... We've encountered an error finding this user</div>;
-    }
+  if (!user) {
+    return <div>Uh. oh... We've encountered an error finding this user</div>;
+  }
 
-    
-  
-    return (
-      <>
-        <div className="userProfileContainer">
-        <div className='userProfileContent'>
-          
+  return (
+    <>
+      <div className="userProfileContainer">
+        <div className="userProfileContent">
           <div>{user.firstName}</div>
           <div>{user.lastName}</div>
           <img src={user.photo} alt={''} />
           <div>{user.bio}</div>
+          {user.member ? (
+            <Divider>
+              <Chip label="TEAM" />
+            </Divider>
+          ) : (
+            <Divider />
+          )}
           <div>{user.member}</div>
           <div>{user.email}</div>
           <div>{user.reviewCount}</div>
@@ -36,11 +38,10 @@ const ProfileUser = () => {
           {/* <div>{user.endorsements.map((endorsement) => (
             <div key={endorsement.id}>{endorsement.name}</div>
           ))}</div> */}
-          </div>
         </div>
-      </>
-    );
-  };
-  
-  export default ProfileUser;
-  
+      </div>
+    </>
+  );
+};
+
+export default ProfileUser;
