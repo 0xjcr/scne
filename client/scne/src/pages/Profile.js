30bb4ae18017a87b44119b63d9e5219d66d7f6ec
Jordan Rollins <carpeomniavino@gmail.com>
2023-04-21 10:05:05 +0200

    removed ugly back buttons

--- client/scne/src/pages/Profile.js
+++ client/scne/src/pages/Profile.js
@@ -1,19 +1,13 @@
-import EditUser from '../components/EditUser';
 import Navbar from '../components/Navbar';
 import ProfileUser from '../components/ProfileUser';
-import { useNavigate } from 'react-router-dom';
+
 
 const Profile = () => {
-
-    const navigate = useNavigate();
-    const goBack = () => {
-        navigate(-1);
-    };
 
 
   return (
     <>
-    <button onClick={goBack}> BACK</button>
+    
 <ProfileUser></ProfileUser>
 <Navbar/>
     
--- client/scne/src/pages/ProfileBusiness.js
+++ client/scne/src/pages/ProfileBusiness.js
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getBusiness } from "../api-service";
 import BusinessProfile from "../components/BusinessProfile";
@@ -8,18 +8,12 @@ const ProfileBusiness = () => {
   const { id } = useParams();
   const [business, setBusiness] = useState(null);
 
-  const navigate = useNavigate();
-  const goBack = () => {
-      navigate(-1);
-};
-
   useEffect(() => {
     getBusiness(id).then((profile) => setBusiness(profile));
   }, [id]);
 
   return (
     <>
-    <button onClick={goBack}>BACK</button>
       {business ? (
         <BusinessProfile
           name={business.name}
