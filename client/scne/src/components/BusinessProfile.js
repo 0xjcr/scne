b8a527b7a40772cbdbb2be65cf820c652a2f5e27
Jordan Rollins <carpeomniavino@gmail.com>
2023-04-25 16:55:26 +0200

    ui almsot complete: to add business ig

--- client/scne/src/components/BusinessProfile.js
+++ client/scne/src/components/BusinessProfile.js
@@ -5,6 +5,8 @@ import Divider from '@mui/material/Divider';
 import Chip from '@mui/material/Chip';
 import IconButton from '@mui/material/IconButton';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 
 const BusinessProfile = ({ name, bio, city, address, phone, reviewCount, ig, photo }) => {
@@ -19,53 +21,68 @@ const BusinessProfile = ({ name, bio, city, address, phone, reviewCount, ig, pho
 
   return (
     <>
-      <div className="businessProfileContainer">
-        <div className="businessProfileContent">
-          <div>
-            <h2>{name}</h2>
-            <div>{ig}</div>
-          </div>
-          <div className="busProfilePicCircle">
-          <img className="busProfilePic" src={photo} alt={''} />
-          </div>
-          <div style={{ marginBottom: '20px' }}>{bio}</div>
-          <div>
-            <div>{city}</div>
-            <div>{address}</div>
-            <div style={{ marginBottom: '20px' }}>{phone}</div>
-            
-          </div>
-          <div>{reviewCount}</div>
-          <Divider>
-            <Chip label="TEAM" />
-          </Divider>
-          <div className="circleUserList" style={{ display: 'flex', flexDirection: 'row' }}>
-            {users.map((user) => (
-              <CircleUser
-                key={user.id}
-                id={user.id}
-                firstName={user.firstName}
-                reviewCount={user.reviewCount}
-                photo={user.photo}
-                clickable={true}
-                member={user.member}
-              />
-            ))}
-          </div>
-          <IconButton aria-label="review" sx={{ transform: 'scale(2)', position: 'absolute', bottom: '50px', right: 
-          '40px' }} 
-          
-          // onClick={handleUpvote}
-          
-          >
-
-
-  <AutoAwesomeIcon sx={{ color: "#70FF00" }}/></IconButton>
-        </div>
+      <div>
+        <Card
+          className="card"
+          sx={{ maxWidth: '90vw', margin: '5vw', borderRadius: '30px', background: 'transparent', border: '1px solid' }}
+          style={{ color: 'whitesmoke' }}
+        >
+          <CardContent>
+            <div className="businessProfileContent">
+              <div>
+                <h2>{name}</h2>
+                <div>{ig}</div>
+              </div>
+              <Divider />
+              <div className="busProfilePicCircle">
+                <img className="busProfilePic" src={photo} alt={''} />
+              </div>
+              <div style={{ marginBottom: '20px' }}>{bio}</div>
+              <Divider />
+              <div>
+                <div>{city}</div>
+                <div>{address}</div>
+                <div style={{ marginBottom: '20px' }}>{phone}</div>
+              </div>
+              <Divider />
+              <div>{reviewCount}</div>
+              <Divider>
+                <Chip label="TEAM" sx={{ color: 'black', backgroundColor: 'lightgrey' }} />
+              </Divider>
+              <div className="circleUserList" style={{ display: 'flex', flexDirection: 'row' }}>
+                {users.map((user) => (
+                  <CircleUser
+                    key={user.id}
+                    id={user.id}
+                    firstName={user.firstName}
+                    reviewCount={user.reviewCount}
+                    photo={user.photo}
+                    clickable={true}
+                    member={user.member}
+                  />
+                ))}
+              </div>
+              <Divider />
+              <div>
+                <p>Hours of Operation</p>
+                <p>Monday - Sunday 00:00 - 24:00</p>
+              </div>
+              <IconButton
+                aria-label="review"
+                sx={{ transform: 'scale(2)', position: 'absolute', bottom: '50px', right: '40px' }}
+                // onClick={handleUpvote}
+              >
+                <AutoAwesomeIcon sx={{ color: '#70FF00' }} />
+              </IconButton>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </>
   );
 };
+
+
 
 export default BusinessProfile;
 
