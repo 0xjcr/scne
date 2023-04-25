d89e291493a1e29fa4994000cd3fe692dcb06748
Jordan Rollins <carpeomniavino@gmail.com>
2023-04-25 19:09:33 +0200

    hover::

--- client/scne/src/components/Topbar.js
+++ client/scne/src/components/Topbar.js
@@ -5,21 +5,16 @@ import SpeedDialAction from '@mui/material/SpeedDialAction';
 import Box from '@mui/material/Box';
 import { ReactComponent as UfoLogo } from '../ufologo.svg';
 
-const StyledSpeedDial = styled(SpeedDial)(({theme }) => ({
+const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
   position: 'absolute',
   left: '0.1rem',
-  backgroundColor: 'transparent!',
-  boxShadow: 'none',
   '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
     bottom: theme.spacing(2),
     right: theme.spacing(2),
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
   },
   '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
     top: theme.spacing(2),
     left: theme.spacing(2),
-    backgroundColor: 'transparent',
   },
   '& .MuiSpeedDial-fab': {
     backgroundColor: 'transparent',
@@ -30,9 +25,9 @@ const StyledSpeedDial = styled(SpeedDial)(({theme }) => ({
       boxShadow: 'none',
     },
     '&:active': {
-      backgroundColor: 'transparent',
-      boxShadow: 'none',
-    },
+
+      
+  },
   },
   '& .MuiSpeedDialAction-staticTooltipLabel': {
     // width: 'auto',
@@ -41,13 +36,39 @@ const StyledSpeedDial = styled(SpeedDial)(({theme }) => ({
     // backgroundColor: '#f7f7f7',
     // color: '#333',
     // fontWeight: 'bold',
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
+   
   },
   '& .MuiSpeedDialAction-fab': {
     backgroundColor: 'transparent',
     boxShadow: 'none',
+    
   },
+  '& .MuiSpeedDialAction-fab:active': {
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    
+  },
+  
+  '& .MuiSpeedDialAction-fab:hover': {
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    
+  },
+  '& .MuiSpeedDialAction-fab:active': {
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    
+  },
+  '& .MuiSpeedDialAction-fab:active': {
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    
+  },
+
+  '& .css-1l2bu63-MuiSpeedDial-root': {
+    
+  },
+  
 }));
 const Topbar = ({ scene, onSceneChange }) => {
   const handleChange = (value) => {
@@ -58,6 +79,7 @@ const Topbar = ({ scene, onSceneChange }) => {
     <>
       <div className="topbar">
         <StyledSpeedDial 
+        
           ariaLabel="SpeedDial"
           icon={
             <Box sx={{ bgcolor: 'transparent', p: 0.1, borderRadius: '10%' , fontSize: '1.87rem'}}>SCNE
@@ -75,12 +97,14 @@ const Topbar = ({ scene, onSceneChange }) => {
   sx={{
     fontSize: scene === 'mixology' ? '2rem' : '1.rem',
     color: 'white',
-    backgroundColor: 'transparent',
     border: 'none',
-    boxShadow: 'none',
     width: '80px',
     height: '40px',
     minWidth: '80px',
+    selected: {
+      color: 'white',
+  
+  }
   }}
   onClick={() => handleChange('mixology')}
 />
@@ -96,12 +120,18 @@ const Topbar = ({ scene, onSceneChange }) => {
     width: '80px',
     height: '40px',
     minWidth: '80px',
+    selected: {
+      backgroundColor: 'transparent',
+      color: 'white',
+      boxShadow: 'none',
+  }
   }}
   onClick={() => handleChange('wellness')}
 />
 <SpeedDialAction
   icon={'coffee'}
   tooltipTitle="coffee"
+   selected={scene === 'coffee'}
   sx={{
     fontSize: scene === 'coffee' ? '2rem' : '1.rem',
     color: 'white',
@@ -111,9 +141,14 @@ const Topbar = ({ scene, onSceneChange }) => {
     width: '80px',
     height: '40px',
     minWidth: '80px',
+    selected: {
+      backgroundColor: 'transparent',
+      color: 'white',
+      boxShadow: 'none',
+  }
   }}
   onClick={() => handleChange('coffee')}
-  selected={scene === 'coffee'}
+ 
 />
 </StyledSpeedDial>
 </div>
