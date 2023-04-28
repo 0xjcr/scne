93d9a8ad09adaf31a2c5c92ba5cdcbdc026edb4c
Rosie Dyvig <rosiedyvig@gmail.com>
2023-04-28 13:33:06 +0200

    feat: join endpoint tests

--- server/controllers/userProfileController.test.js
+++ server/controllers/userProfileController.test.js
@@ -4,7 +4,7 @@ const supertest = require("supertest");
 const { Users } = require("../models/users");
 const sequelize = require("../models");
 
-describe("Intergration Tests", () => {
+describe("/join endpoint", () => {
   const app = express();
   app.use(express.json());
   app.use(router);
@@ -57,7 +57,97 @@ describe("Intergration Tests", () => {
     const res = await request.post("/join").send(user);
 
     expect(res.status).toBe(201);
-    //expect(res.body.error).toBe(false);
-    expect(res.body.message).toBe(user);
   });
+  it("should not return the password in the response", async () => {
+    const user = {
+      firstName: "John",
+      lastName: "Doe",
+      city: "New York",
+      ig: "johndoe",
+      email: "johndoe@example.com",
+      password: "abc",
+      bio: "This is my bio",
+    };
+
+    const res = await request.post("/join").send(user);
+
+    expect(res.body.password).toBe(undefined);
+  });
+});
+
+describe("/ endpoint for login", () => {
+  const app = express();
+  app.use(express.json());
+  app.use(router);
+  const request = supertest(app);
+
+  beforeAll(async () => {
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    try {
+      await Users.destroy({ where: {}, truncate: true, cascade: true });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  it("should search the db for the email adress", async () => {
+    await request.post("/join").send({
+      firstName: "John",
+      lastName: "Doe",
+      city: "New York",
+      ig: "johndoe",
+      email: "johndoe@example.com",
+      password: "abc",
+      bio: "This is my bio",
+    });
+
+    await request.post("/").send({
+      email: "johndoe@example.com",
+      password: "abc",
+    });
+    const user = await Users.findOne({
+      where: { email: "johndoe@example.com" },
+    });
+
+    expect(user.email).toBe("johndoe@example.com");
+  });
+  it("should return 403 if email does not match", async () => {
+    await request.post("/join").send({
+      firstName: "John",
+      lastName: "Doe",
+      city: "New York",
+      ig: "johndoe",
+      email: "johndoe@example.com",
+      password: "abc",
+      bio: "This is my bio",
+    });
+
+    await request.post("/").send({
+      email: "wrong@example.com",
+      password: "abc",
+    });
+    const user = await Users.findOne({
+      where: { email: "wrong@example.com" },
+    });
+    expect(request.status).toBe(404);
+    expect(request.body.message).toBe("Email not found");
+  });
+  // it("should return 404 if username is not found", async () => {
+  //   const res = await request.post("/").send({
+  //     email: "johndoe@example.com",
+  //     password: "abc",
+  //   });
+  //   const user = await Users.findOne({
+  //     where: { email: "wrong@example.com" },
+  //   });
+  //   const bizs = await Bizs.findOne({ where: { email: "wrong@example.com" } });
+
+  //   expect(res.status).toBe(404);
+  //   expect(res.body.status).toBe(404);
+  //   expect(res.body.data).toBe(null);
+  //   expect(res.body.error).toStrictEqual({});
+  //   expect(res.body.message).toBe("Email not found");
+  // });
 });
