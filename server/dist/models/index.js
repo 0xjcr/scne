cf18bee2fe911ed7e357cb5ac149903543e4ee01
Rosie Dyvig <rosiedyvig@gmail.com>
2023-05-03 16:58:49 +0200

    dist/models/index error

--- server/dist/models/index.js
+++ server/dist/models/index.js
@@ -2,15 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const { NODE_ENV } = process.env;
-const sequelize = new sequelize_1.Sequelize(
-  NODE_ENV !== "TESTING" ? "scne" : "scene_test",
-  "jamesbratton",
-  "",
-  {
+const sequelize = new sequelize_1.Sequelize(NODE_ENV !== "TESTING" ? "scne" : "scene_test", "jamesbratton", "", {
     host: "localhost",
     dialect: "postgres",
     port: 5432,
     logging: false,
-  }
-);
+});
 exports.default = sequelize;
