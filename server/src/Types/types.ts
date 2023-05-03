b98b4569d25fe963026c1d4c621d9d140d366d13
Rosie Dyvig <rosiedyvig@gmail.com>
2023-05-03 18:59:54 +0200

    update Profile test

--- server/src/Types/types.ts
+++ server/src/Types/types.ts
@@ -1,4 +1,5 @@
 export type UserType = {
+  id?: number;
   firstName: string;
   lastName: string;
   city: string;
--- server/src/__tests__/userProfileController.test.ts
+++ server/src/__tests__/userProfileController.test.ts
@@ -22,10 +22,6 @@ describe("/join endpoint", () => {
       console.log(error);
     }
   });
-  it("should pass", () => {
-    expect(true).toBe(true);
-  });
-
   it("should save a user to the database", async () => {
     const user: UserType = {
       firstName: "Nobody",
@@ -40,227 +36,108 @@ describe("/join endpoint", () => {
     await request.post("/join").send(user);
 
     const result = (await (Users.findAll() as unknown)) as UserType[];
-    expect(result[0].firstName).toBe("Test Business");
-    expect(result[0].city).toEqual("Test City");
-    expect(result[0].address).toEqual("Test Address");
-    expect(result[0].phone).toEqual("123-456-7890");
-    expect(result[0].ig).toEqual("test_ig");
-    expect(result[0].email).toEqual("test@test.com");
-    expect(result[0].scene).toEqual("Test Scene");
-    expect(result[0].bio).toEqual("Test bio");
-    expect(result[0].photo).toEqual("test.jpg");
+    expect(result[0].firstName).toBe("Nobody");
+    expect(result[0].lastName).toBe("CoolGuy");
+    expect(result[0].city).toBe("The Moon");
+    expect(result[0].ig).toBe("ig");
+    expect(result[0].email).toBe("nobody@themoon.com");
+    expect(result[0].bio).toBe("this is my moon");
   });
-  it("should return 201 if business is created", async () => {
-    const biz: BizType = {
-      name: "Test Business",
-      city: "Test City",
-      address: "Test Address",
-      phone: "123-456-7890",
-      reviewCount: "17",
-      upvotes: "3",
-      ig: "test_ig",
-      email: "test@test.com",
+  it("should return 201 if user is created", async () => {
+    const user: UserType = {
+      firstName: "Nobody",
+      lastName: "CoolGuy",
+      city: "The Moon",
+      ig: "ig",
+      email: "nobody@themoon.com",
       password: "password",
-      scene: "Test Scene",
-      bio: "Test bio",
-      photo: "test.jpg",
+      bio: "this is my moon",
     };
 
-    const res = await request.post("/joinbiz").send(biz);
+    const res = await request.post("/join").send(user);
     expect(res.status).toBe(201);
   });
 
-  //   it("should save a user to the database", async () => {
-  //     const user = {
-  //       firstName: "John",
-  //       lastName: "Doe",
-  //       city: "New York",
-  //       ig: "johndoe",
-  //       email: "johndoe@example.com",
-  //       password: "abc",
-  //       bio: "This is my bio",
-  //     };
+  it("should not return the password in the response", async () => {
+    const user = {
+      firstName: "John",
+      lastName: "Doe",
+      city: "New York",
+      ig: "johndoe",
+      email: "johndoe@example.com",
+      password: "abc",
+      bio: "This is my bio",
+    };
 
-  //     await request.post("/join").send(user);
+    const res = await request.post("/join").send(user);
 
-  //     const result = await Users.findOne({ user });
-  //     expect(result.firstName).toBe("John");
-  //     expect(result.lastName).toEqual("Doe");
-  //     expect(result.city).toEqual("New York");
-  //     expect(result.ig).toEqual("johndoe");
-  //     expect(result.email).toEqual("johndoe@example.com");
-  //     expect(result.bio).toEqual("This is my bio");
-  //   });
-  //   it("should return 201 if user is created", async () => {
-  //     const user = {
-  //       firstName: "John",
-  //       lastName: "Doe",
-  //       city: "New York",
-  //       ig: "johndoe",
-  //       email: "johndoe@example.com",
-  //       password: "abc",
-  //       bio: "This is my bio",
-  //     };
+    expect(res.body.password).toBe(undefined);
+  });
+});
 
-  //     const res = await request.post("/join").send(user);
+describe("update profile", () => {
+  const app = express();
+  app.use(express.json());
+  app.use(router);
+  const request = supertest(app);
 
-  //     expect(res.status).toBe(201);
-  //   });
-  //   it("should not return the password in the response", async () => {
-  //     const user = {
-  //       firstName: "John",
-  //       lastName: "Doe",
-  //       city: "New York",
-  //       ig: "johndoe",
-  //       email: "johndoe@example.com",
-  //       password: "abc",
-  //       bio: "This is my bio",
-  //     };
+  beforeAll(async () => {
+    await sequelize.sync();
 
-  //     const res = await request.post("/join").send(user);
+    const user: UserType = {
+      firstName: "Nobody",
+      lastName: "CoolGuy",
+      city: "The Moon",
+      ig: "ig",
+      email: "nobody@themoon.com",
+      password: "password",
+      bio: "this is my moon",
+    };
 
-  //     expect(res.body.password).toBe(undefined);
-  //   });
-  // });
+    await request.post("/join").send(user);
+  });
 
-  // describe("/ endpoint for login", () => {
-  //   const app = express();
-  //   app.use(express.json());
-  //   app.use(router);
-  //   const request = supertest(app);
+  afterEach(async () => {
+    try {
+      await Users.destroy({ where: {}, truncate: true, cascade: true });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
-  //   beforeAll(async () => {
-  //     await sequelize.sync();
-  //   });
+  it("it should allow a user to update their profile", async () => {
+    const user = (await Users.findOne({
+      where: { firstName: "Nobody" },
+    })) as unknown as UserType;
 
-  //   afterEach(async () => {
-  //     try {
-  //       await Users.destroy({ where: {}, truncate: true, cascade: true });
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   });
-  //   it("should search the db for the email address", async () => {
-  //     await request.post("/join").send({
-  //       firstName: "John",
-  //       lastName: "Doe",
-  //       city: "New York",
-  //       ig: "johndoe",
-  //       email: "johndoe@example.com",
-  //       password: "abc",
-  //       bio: "This is my bio",
-  //     });
+    const newProfile = {
+      photo: "new_photo.jpg",
+      bio: "new_bio",
+      ig: "new_ig",
+    };
 
-  //     await request.post("/").send({
-  //       email: "johndoe@example.com",
-  //       password: "abc",
-  //     });
-  //     const user = await Users.findOne({
-  //       where: { email: "johndoe@example.com" },
-  //     });
+    const res = await request.put(`/editprofile/${user.id}`).send(newProfile);
 
-  //     expect(user.email).toBe("johndoe@example.com");
-  //   });
-  //   it("should return 400 if email is not found", async () => {
-  //     await request.post("/join").send({
-  //       firstName: "John",
-  //       lastName: "Doe",
-  //       city: "New York",
-  //       ig: "johndoe",
-  //       email: "johndoe@example.com",
-  //       password: "abc",
-  //       bio: "This is my bio",
-  //     });
+    expect(res.status).toBe(200);
+    expect(res.body.firstName).toBe("Nobody");
+    expect(res.body.lastName).toBe("CoolGuy");
+    expect(res.body.photo).toBe("new_photo.jpg");
+    expect(res.body.ig).toBe("new_ig");
+    expect(res.body.bio).toBe("new_bio");
+  });
 
-  //     await request.post("/").send({
-  //       email: "wrong@example.com",
-  //       password: "abc",
-  //     });
-  //     await Users.findOne({
-  //       where: { email: "wrong@example.com" },
-  //     });
+  it("should return 400 if profile id is invalid", async () => {
+    const user = { id: 10000 };
 
-  //     await Bizs.findOne({
-  //       where: { email: "wrong@example.com" },
-  //     });
-  //     expect(request.status).toBe(400);
-  //     expect(request.body.message).toBe("Email not found");
-  //   });
-  // });
+    const newProfile = {
+      photo: "this shouldn't work",
+      bio: "this shouldn't work",
+      ig: "this shouldn't work",
+    };
 
-  // describe("/logout", () => {
-  //   let req;
-  //   let res;
+    const res = await request.put(`/editprofile/${user.id}`).send(newProfile);
 
-  //   beforeEach(() => {
-  //     req = {
-  //       session: {
-  //         destroy: jest.fn(),
-  //       },
-  //     };
-  //     res = {
-  //       status: jest.fn().mockReturnThis(),
-  //       json: jest.fn(),
-  //     };
-  //   });
-
-  //   it('should respond with 200 and message "Logged out successfully" if session destroy succeeds', () => {
-  //     logout(req, res);
-  //     expect(req.session.destroy).toHaveBeenCalled();
-  //     expect(res.status).toHaveBeenCalledWith(200);
-  //     expect(res.json).toHaveBeenCalledWith({
-  //       message: "Logged out successfully",
-  //     });
-  //   });
-
-  //   it('should respond with 500 and message "Error logging out" if session destroy fails', () => {
-  //     const error = new Error("test error");
-  //     req.session.destroy.mockImplementationOnce((callback) => callback(error));
-
-  //     logout(req, res);
-
-  //     expect(req.session.destroy).toHaveBeenCalled();
-  //     expect(res.status).toHaveBeenCalledWith(500);
-  //     expect(res.json).toHaveBeenCalledWith({ message: "Error logging out" });
-  //   });
-  // });
-
-  // describe("updateProfile", () => {
-  //   let req;
-  //   let res;
-
-  //   beforeEach(() => {
-  //     req = {
-  //       params: { id: 1 },
-  //       body: {
-  //         photo: "test-photo.jpg",
-  //         bio: "test bio",
-  //         ig: "test-ig",
-  //         member: true,
-  //         scene0: "test scene0",
-  //         scene1: "test scene1",
-  //         scene2: "test scene2",
-  //         endorsed: true,
-  //       },
-  //     };
-  //     res = {
-  //       status: jest.fn().mockReturnThis(),
-  //       json: jest.fn(),
-  //     };
-  //   });
-
-  //   afterEach(() => {
-  //     jest.clearAllMocks();
-  //   });
-
-  //   it("should respond with status 400 and an error message if an error occurs", async () => {
-  //     jest.spyOn(Users, "findByPk").mockImplementationOnce(() => {
-  //       throw new Error("Test error");
-  //     });
-
-  //     await updateProfileAlt(req, res);
-
-  //     expect(res.status).toHaveBeenCalledWith(400);
-  //     expect(res.json).toHaveBeenCalledWith({ message: "Test error" });
-  //   });
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBeDefined();
+  });
 });
